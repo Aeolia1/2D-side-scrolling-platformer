61611eae87738bffc6f026bdd6e976ed8ab10d5f
Xiuw11 <51129854+Aeolia1@users.noreply.github.com>
2022-08-12 19:25:20 +0100

    Add files via upload

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] int playerLives = 3;
+    [SerializeField] TextMeshProUGUI livesText;
+    protected AudioSource deathAudio; //luo
+
+    void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        deathAudio = GetComponent<AudioSource>(); //luo
+        deathAudio.Play(); //luo
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+        DontDestroyOnLoad(gameObject);
+        }
+    }
+        
+    void Start()
+    {
+        livesText.text = playerLives.ToString();
+    }
+
+
+    public void ProcessPlayerDeath()
+    {
+        deathAudio = GetComponent<AudioSource>(); //luo
+        deathAudio.Play(); //luo
+        if (playerLives > 0)
+        {
+            TakeLife();
+        }
+        else
+        {
+            Reset0();
+        } 
+    }
+
+    void TakeLife()
+    {
+        playerLives--;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playerLives.ToString();
+    }
+
+
+
+    void Reset0()
+    {
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 1f;
+    protected AudioSource nextLevel; //luo
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(LoadNextLevel());
+            nextLevel = GetComponent<AudioSource>(); //luo
+            nextLevel.Play(); //luo
+        }
+        
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+        
+}
